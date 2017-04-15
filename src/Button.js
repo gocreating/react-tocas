9917db2b01ea85bb83ced7ea54acb164cd46e05a
翁治平 <gocreating@gmail.com>
2017-04-16 02:05:03 +0800

    Add component `Button`

---
+++ src/Button.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import Box from './Box';
+
+class Button extends Component {
+  render() {
+    let { className, ...rest } = this.props;
+    let cx = classNames(
+      className,
+      'button'
+    );
+
+    return (
+      <Box
+        wrapper="button"
+        ts
+        className={cx}
+        {...rest}
+      />
+    );
+  }
+}
+
+export default Button;
