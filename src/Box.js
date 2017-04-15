e93692ab20a4ab703eee64bff6cf2e77b2f69df1
翁治平 <gocreating@gmail.com>
2017-04-16 02:04:38 +0800

    Add component `Box`

---
+++ src/Box.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const tocasProps = [
+  // main
+  'ts',
+  // colors
+  'primary', 'info', 'warning', 'positive', 'negative', 'inverted',
+  // sizes
+  'mini', 'tiny', 'small', 'medium', 'large', 'big', 'huge', 'massive',
+];
+
+class Box extends Component {
+  render() {
+    let {
+      wrapper,
+      className,
+      children,
+      ...rest
+    } = this.props;
+    let cx = classNames(
+      className,
+      ...tocasProps.map(tocasProp => {
+        delete rest[tocasProp];
+
+        return {
+          [tocasProp]: this.props[tocasProp],
+        };
+      })
+    );
+
+    return React.createElement(
+      wrapper,
+      {
+        className: cx,
+        ...rest,
+      },
+      children
+    );
+  }
+}
+
+Box.defaultProps = {
+  wrapper: 'div',
+};
+
+Box.propTypes = {
+  wrapper: PropTypes.node.isRequired,
+  ...Object.assign({},
+    ...tocasProps.map(tocasProp => ({ [tocasProp]: PropTypes.bool }))
+  ),
+};
+
+export default Box;
