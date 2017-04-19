2e3d6cce86a260a277fb966d761a8103eaceaf6b
翁治平 <gocreating@gmail.com>
2017-04-20 04:23:43 +0800

    Add Slate

---
+++ src/Slate.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Box from './Box';
+
+class Slate extends Component {
+  render() {
+    let {
+      basic,
+      heading,
+      dashed,
+      borderless,
+      fluid,
+      insetted,
+      clickable,
+      compact,
+      className,
+      ...rest
+    } = this.props;
+    let cx = classNames(
+      {
+        basic,
+        heading,
+        dashed,
+        borderless,
+        fluid,
+        insetted,
+        clickable,
+        compact,
+      },
+      'slate',
+      className
+    );
+
+    return (
+      <Box
+        ts
+        className={cx}
+        {...rest}
+      />
+    );
+  }
+}
+
+Slate.displayName = 'Slate';
+
+Slate.propTypes = {
+  basic: PropTypes.bool,
+  heading: PropTypes.bool,
+  dashed: PropTypes.bool,
+  borderless: PropTypes.bool,
+  fluid: PropTypes.bool,
+  insetted: PropTypes.bool,
+  clickable: PropTypes.bool,
+  compact: PropTypes.bool,
+};
+
+export default Slate;
