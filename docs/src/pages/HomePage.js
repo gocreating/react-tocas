b59f8ff27613694d09326e91cc850d639c83abda
翁治平 <gocreating@gmail.com>
2017-04-21 05:27:27 +0800

    Add disqus thread to homepage of doc-site

--- docs/src/pages/HomePage.js
+++ docs/src/pages/HomePage.js
@@ -5,6 +5,11 @@ import tocasOg from '../../public/tocas-og.png';
 let HomePage = () => (
   <PageLayout>
     <img className="ts fluid image" src={tocasOg} alt="logo" />
+    <ReactDisqusThread
+      shortname="react-tocas"
+      identifier="homepage"
+      title="Welcome to React-Tocas"
+    />
   </PageLayout>
 );
 
