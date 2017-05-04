1fd88e1b080b47ed7a983070f880dfb013671b47
翁治平 <gocreating@gmail.com>
2017-05-05 01:08:15 +0800

    Update Icon

--- src/Icon.js
+++ src/Icon.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import Box from './Box';
 
 class Icon extends Component {
   render() {
     let {
       name,
+      link,
+      circular,
       className,
       ...rest
     } = this.props;
     let cx = classNames(
       name,
+      {
+        link,
+        circular,
+      },
       'icon',
       className
     );
 
     return (
-      <i
+      <Box
+        wrapper="i"
         className={cx}
         {...rest}
       />
@@ -28,6 +36,8 @@ Icon.displayName = 'Icon';
 
 Icon.propTypes = {
   name: PropTypes.string.isRequired,
+  link: PropTypes.bool,
+  circular: PropTypes.bool,
 };
 
 export default Icon;
