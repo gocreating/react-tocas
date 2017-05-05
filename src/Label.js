fdd5c164c0afb5aaceb706e08d3e5daa6a9c9288
翁治平 <gocreating@gmail.com>
2017-05-05 15:37:55 +0800

    Update Label

--- src/Label.js
+++ src/Label.js
@@ -7,12 +7,26 @@ class Label extends Component {
   render() {
     let {
       basic,
+      image,
+      circular,
+      horizontal,
+      floating,
+      empty,
+      triangle,
+      square,
       className,
       ...rest
     } = this.props;
     let cx = classNames(
       {
         basic,
+        image,
+        circular,
+        horizontal,
+        floating,
+        empty,
+        triangle,
+        square,
       },
       'label',
       className
@@ -32,6 +46,13 @@ Label.displayName = 'Label';
 
 Label.propTypes = {
   basic: PropTypes.bool,
+  image: PropTypes.bool,
+  circular: PropTypes.bool,
+  horizontal: PropTypes.bool,
+  floating: PropTypes.bool,
+  empty: PropTypes.bool,
+  triangle: PropTypes.bool,
+  square: PropTypes.bool,
 };
 
 export default Label;
